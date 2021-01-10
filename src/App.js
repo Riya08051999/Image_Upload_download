4bf589e29c3ce7554f29bf19b2f3a1e9e6d1c689
Riya <abc@gmail.com>
2021-01-10 12:50:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { app } from "./base";
+
+const db = app.firestore();
 
 function App() {
+  const [fileUrl, setFileUrl] = React.useState(null);
+  const [users, setUsers] = React.useState([]);
+
+  const onFileChange = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl(await fileRef.getDownloadURL());
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    if (!username || !fileUrl) {
+      return;
+    }
+    await db.collection("image").doc(username).set({
+      name: username,
+      avatar: fileUrl,
+    });
+  };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersCollection = await db.collection("image").get();
+      setUsers(
+        usersCollection.docs.map((doc) => {
+          return doc.data();
+        })
+      );
+    };
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="file" onChange={onFileChange} />
+        <input type="text" name="username" placeholder="NAME" />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.name}>
+              <img width="500" height="500" src={user.avatar} alt={user.name} />
+              <p>{user.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
---
+++ src/base.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+import 'firebase/storage'
+
+export const app = firebase.initializeApp({
+    "projectId": "image-9ea93",
+    "appId": "1:913523709332:web:c5364cc03820e84f51ab81",
+    "storageBucket": "image-9ea93.appspot.com",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyAVnINmse8f_-2xDFKT5i4-gnmw4eKMH-w",
+    "authDomain": "image-9ea93.firebaseapp.com",
+    "messagingSenderId": "913523709332",
+    "measurementId": "G-DWHP5J763B"
+  });
